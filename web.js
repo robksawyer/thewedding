b540348b7d64d3b5ee63f76cd7131cb3fe00e47c
Rob Sawyer <robksawyer@gmail.com>
2016-02-08 11:50:10 -0800

    Trying to run app on heroku

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require("express");
 
 var app = express();
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');

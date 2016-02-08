e2363068130676914db0a3703204166c6b39ccea
Rob Sawyer <robksawyer@gmail.com>
2016-02-08 11:35:10 -0800

    Trying to run app on heroku

--- web.js
+++ web.js
@@ -2,9 +2,7 @@ var express = require("express");
 
 var app = express();
 
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(express.static(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 console.log('Port ' + port);
